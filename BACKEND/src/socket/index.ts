3fb6fce99baaf970fd7c935e1cbbea6450ad104b
aymaan <aymaanpathan5@gmail.com>
2025-11-14 23:34:56 +0530

    feat: add startSession socket event to handle user session initiation

--- BACKEND/src/socket/index.ts
+++ BACKEND/src/socket/index.ts
@@ -3,12 +3,14 @@ import { joinRoom } from "./joinRoom";
 import { codeChange } from "./codeChange";
 import { cursorMove } from "./cursorMove";
 import { disconnect } from "./disconnect";
+import { startSession } from "./startSession";
 
 export const registerSocketEvents = (io: Server) => {
   io.on("connection", (socket: Socket) => {
     console.log("New user connected:", socket.id);
 
     joinRoom(socket);
+    startSession(socket);
     codeChange(socket);
     cursorMove(socket);
     disconnect(socket);
